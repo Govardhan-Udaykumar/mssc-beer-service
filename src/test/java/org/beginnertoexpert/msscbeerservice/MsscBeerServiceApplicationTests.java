59786a6b86f874ccabcec21d27ca2ec78b8c691b
govardhanAjackus <govardhan.u@ajackus.com>
2024-04-22 10:51:02 +0530

    Created initial project for Beer Service. Closes #1

---
+++ src/test/java/org/beginnertoexpert/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.beginnertoexpert.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
