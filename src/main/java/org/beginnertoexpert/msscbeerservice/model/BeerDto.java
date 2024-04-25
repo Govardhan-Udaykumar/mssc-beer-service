c5dcf78fb1b09aacd66aa9228ce6c371b61f3844
govardhanAjackus <govardhan.u@ajackus.com>
2024-04-25 11:39:37 +0530

    Added bean validation for BeerDto. Closes #5

--- src/main/java/org/beginnertoexpert/msscbeerservice/model/BeerDto.java
+++ src/main/java/org/beginnertoexpert/msscbeerservice/model/BeerDto.java
@@ -1,5 +1,9 @@
 package org.beginnertoexpert.msscbeerservice.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,14 +19,35 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    @Positive
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @NotNull
+    @Positive
     private Long upc;
+
+    @NotNull
+    @Positive
     private BigDecimal price;
+
+    @NotNull
+    @Positive
     private Integer quantityOnHand;
 
 }
